92f0edcdb696998005cc6b569caaab90fb30db74
yadla-akshitha <akshithareddy.yadla@gmail.com>
2025-02-11 10:43:42 +0530

    Build a GET API

---
+++ learn-spring-framework/learn-spring-framework/src/main/java/com/microservices/spring/learn_spring_framework/CurrencyConfigurationController.java
@@ -0,0 +1,22 @@
+package com.microservices.spring.learn_spring_framework;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyConfigurationController {
+	
+	@Autowired
+	private CurrencyServiceConfiguration configuration;
+	
+	@RequestMapping("/currency-configuration")
+	public CurrencyServiceConfiguration retrieveAllCourses() {
+		return configuration;
+	}
+
+}
