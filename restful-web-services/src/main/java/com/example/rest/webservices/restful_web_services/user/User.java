0fe1d3ab70398c26919c4c5c83ef56d685a7d32c
yadla-akshitha <akshithareddy.yadla@gmail.com>
2025-02-13 11:09:19 +0530

    Filtering

--- restful-web-services/src/main/java/com/example/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/example/rest/webservices/restful_web_services/user/User.java
@@ -2,6 +2,8 @@ package com.example.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -10,9 +12,11 @@ public class User {
 	private Integer id;
 	
 	@Size(min = 2, message = "Name should have atleast 2 characters")
+	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message= "Birth Date should be in the past")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
@@ -49,7 +53,4 @@ public class User {
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
 	}
-	
-	
-
 }
