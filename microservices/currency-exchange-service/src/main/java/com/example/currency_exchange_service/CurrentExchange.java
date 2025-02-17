fec924040bfaf308b7d3e8e9ffcac67d0d619785
yadla-akshitha <akshithareddy.yadla@gmail.com>
2025-02-17 14:52:56 +0530

    Currency-exchange-microservice

---
+++ microservices/currency-exchange-service/src/main/java/com/example/currency_exchange_service/CurrentExchange.java
@@ -0,0 +1,54 @@
+package com.example.currency_exchange_service;
+
+import java.math.BigDecimal;
+
+public class CurrentExchange {
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionMultiple;
+
+	public CurrentExchange() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public CurrentExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+
+}
