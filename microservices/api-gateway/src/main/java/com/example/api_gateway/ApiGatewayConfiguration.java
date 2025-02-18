e1a1536fb022e00be3903ad552f376bfb182351c
yadla-akshitha <akshithareddy.yadla@gmail.com>
2025-02-18 13:50:10 +0530

    filters in gateway

---
+++ microservices/api-gateway/src/main/java/com/example/api_gateway/ApiGatewayConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.api_gateway;
+
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+	
+	@Bean
+	RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		
+		return builder.routes()
+				.route(p -> p.path("/get")
+						.filters(f -> f
+								.addRequestHeader("MyHeader", "MYURI"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p.path("/currency-exchange/**")
+						.uri("lb://currency-exchange"))
+				.route(p -> p.path("/currency-conversion/**")
+						.uri("lb://currency-conversion"))
+				.build();
+	}
+
+}
